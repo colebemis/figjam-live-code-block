4797dd1da758a4a1b631880b9cd64e936c9dbace
Cole Bemis <colebemis@github.com>
2021-10-22 16:52:47 -0700

    Move editor script into seperate file

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 const path = require("path");
 
-module.exports = {
+module.exports = (env, argv) => ({
+  // This is necessary because Figma's 'eval' works differently than normal eval
+  devtool: argv.mode === "production" ? false : "inline-source-map",
   entry: {
+    main: "./src/main.tsx",
     editor: "./src/editor.tsx",
   },
   output: {
@@ -13,13 +16,26 @@ module.exports = {
     // https://git.io/JimSo
     publicPath: "/",
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/editor.html",
+      inject: "body",
       filename: "editor.html",
       inlineSource: ".(js)$",
       chunks: ["editor"],
     }),
     new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
   ],
-};
+});
