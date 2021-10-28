05f7974f1012bb7f40e700fa932935d80a5873a8
Cole Bemis <colebemis@github.com>
2021-10-27 21:08:54 -0700

    Add icons

---
+++ widget/src/icons.ts
@@ -0,0 +1,8 @@
+export const playIcon = `<svg viewBox="0 0 32 32" width="32" height="32" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="m13 10.0979 9.4434 5.9021-9.4434 5.9021zm1 1.8042v8.1958l6.5566-4.0979z" fill-rule="evenodd"/></svg>`;
+
+export const plusIcon = `<svg viewBox="0 0 32 32" width="32" height="32" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path d="M16 15V10H17V15H22V16H17V21H16V16H11V15H16Z"/></svg>`;
+
+export const codeIcon = `<svg viewBox="0 0 32 32" width="32" height="32" fill="#fff" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14.8509 22.1139L18.114 10.1491L17.1492 9.88599L13.8861 21.8507L14.8509 22.1139ZM11.7116 12.5916L12.2883 13.4085L8.61699 16L12.2883 18.5916L11.7116 19.4085L6.88293 16L11.7116 12.5916ZM23.383 16L19.7117 13.4085L20.2884 12.5916L25.117 16L20.2884 19.4085L19.7117 18.5916L23.383 16Z"/>
+</svg>
+`;
